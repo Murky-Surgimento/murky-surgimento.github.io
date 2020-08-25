9188104abe6b31e491495672902cf2370cea093e
Colin Brayton <gringolalia@gmail.com>
2020-08-25 17:17:38 -0300

    deploy

---
+++ precache-manifest.53887295505425a8c93fcf78a7a67670.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17bbfa26f3b1fa53f73",
+    "url": "/css/about.efdd2cc1.css"
+  },
+  {
+    "revision": "281f79f4b3c4277de163",
+    "url": "/css/app.a6420a38.css"
+  },
+  {
+    "revision": "1b11b685d97b580639ae",
+    "url": "/css/chunk-vendors.3c76955e.css"
+  },
+  {
+    "revision": "6bc563d217d1120f3807beb34a061d10",
+    "url": "/img/goldengatepaisagem.6bc563d2.jpg"
+  },
+  {
+    "revision": "50b34689795acb1a653ab1825bf8690c",
+    "url": "/img/infernalmachine.50b34689.jpg"
+  },
+  {
+    "revision": "140632208a6a568bda32b00a1671730e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17bbfa26f3b1fa53f73",
+    "url": "/js/about.bf9fb31e.js"
+  },
+  {
+    "revision": "281f79f4b3c4277de163",
+    "url": "/js/app.e9f9f2a6.js"
+  },
+  {
+    "revision": "1b11b685d97b580639ae",
+    "url": "/js/chunk-vendors.0a352330.js"
+  },
+  {
+    "revision": "3c06c2a4b7ac31796c3f5841c9e7d489",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
